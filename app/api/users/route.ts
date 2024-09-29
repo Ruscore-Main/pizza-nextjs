98b9e37d5a3a61c7560315e2457818ae58e021bf
Ruscore-Main <oobitovich@mail.ru>
2024-09-29 22:58:57 +0300

    configured DB + DB design

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -9,6 +9,8 @@ export async function GET() {
 export async function POST(request: NextRequest) {
   const data = await request.json();
 
+  
+
   const user = await prisma.user.create({
     data
   });
